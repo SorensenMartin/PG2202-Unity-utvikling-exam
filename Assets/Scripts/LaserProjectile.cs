db8a1b578eb85a2533294ee868a14494d0b4157b
SanderAakre <sanderjoacimakre@gmail.com>
2023-05-03 17:33:15 +0200

    Projectile, Turret and Desert Rider changes

---
+++ Assets/Scripts/LaserProjectile.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserProjectile : MonoBehaviour
+{
+    public float lifetime = 4;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lifetime -= Time.deltaTime;
+        if ( lifetime <= 0 )
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        Destroy(gameObject);
+    }
+}
