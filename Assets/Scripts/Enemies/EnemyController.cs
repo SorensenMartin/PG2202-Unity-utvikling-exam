83caa2dce5c33f1885be6f3d94a6fe21ba9ffbb5
dennis647 <dennis.brokking@gmail.com>
2023-05-05 16:15:04 +0200

    Enemy Controller
    Updated enemy controller, but still doesn't work like it's supposed to

--- Assets/Scripts/Enemies/EnemyController.cs
+++ Assets/Scripts/Enemies/EnemyController.cs
@@ -2,26 +2,25 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    public float speed = 0.1f;
+    public float speed = 5.0f;
+    public float radius = 10.0f; 
+    public float altitude = 5.0f; 
 
-    public float height = 50f;
+    private Vector3 center;
+    private float angle; 
 
-    public float turnRange = 30f;
+    void Start()
+    {
+        center = transform.position; 
+    }
 
     void Update()
     {
+        angle -= speed * Time.deltaTime;
+        float x = center.x + radius * Mathf.Cos(angle * Mathf.Deg2Rad);
+        float z = center.z + radius * Mathf.Sin(angle * Mathf.Deg2Rad);
+        transform.position = new Vector3(x, altitude, z);
 
-        Vector3 position = transform.position;
-
-        position.y = height;
-
-        position += transform.forward * (speed / 10f) * Time.deltaTime;
-
-        float angle = Random.Range(-turnRange, turnRange);
-
-        transform.Rotate(0f, angle, 0f);
-
-        transform.position = position;
-
+        transform.LookAt(center);
     }
 }
