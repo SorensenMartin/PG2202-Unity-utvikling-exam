aec6d2daa8f994a69680269c87b9ea95a4394972
dennis647 <dennis.brokking@gmail.com>
2023-05-05 16:56:36 +0200

    Finished enemy plane
    Finished code of enemy plane flying around the city

--- Assets/Scripts/Enemies/EnemyController.cs
+++ Assets/Scripts/Enemies/EnemyController.cs
@@ -2,25 +2,32 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    public float speed = 5.0f;
-    public float radius = 10.0f; 
-    public float altitude = 5.0f; 
+    public float radius = 25f;
+    public float speed = 5f;
 
     private Vector3 center;
-    private float angle; 
 
     void Start()
     {
-        center = transform.position; 
+        center = new Vector3(-40, 40, -60);
     }
 
     void Update()
     {
-        angle -= speed * Time.deltaTime;
-        float x = center.x + radius * Mathf.Cos(angle * Mathf.Deg2Rad);
-        float z = center.z + radius * Mathf.Sin(angle * Mathf.Deg2Rad);
-        transform.position = new Vector3(x, altitude, z);
+        float angle = (Time.time * (speed / 10));
+        Vector3 offset = new Vector3(Mathf.Sin(angle), 0, Mathf.Cos(angle)) * radius;
+        transform.position = center + offset;
 
-        transform.LookAt(center);
+        Vector3 nextPosition = center + new Vector3(Mathf.Sin(angle + Time.deltaTime * (speed / 10)), 0, Mathf.Cos(angle + Time.deltaTime * (speed / 10))) * radius;
+
+        Vector3 direction = nextPosition - transform.position;
+
+        if (direction != Vector3.zero)
+        {
+    transform.GetChild(0).rotation = Quaternion.LookRotation(direction, Vector3.up) * Quaternion.Euler(0, 180, 0);
+        }
+
+        transform.position = nextPosition;
+
     }
 }
